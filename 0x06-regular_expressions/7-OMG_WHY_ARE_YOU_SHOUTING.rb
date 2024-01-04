e0e37e85da60b6dadd7e1c1a2034021a9d66721f
sontomtimde <mtimdesonto@yahoo.com>
2024-01-04 04:41:03 -0800

    OMG WHY ARE YOU SHOUTING?

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
-# A regular expression that matches only capital letters
-
-puts ARGV[0].scan(/\b[A-Z]+\b/).join
+# A regular expression that is matches only capital letters
+puts ARGV[0].scan(/[A-Z]/).join
